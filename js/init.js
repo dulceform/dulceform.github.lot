9beae8cfc130cc37f228ffc7cfd503bc25d2ed11
dulceform <74226782+dulceform@users.noreply.github.com>
2021-04-14 00:19:42 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCJ0NRYERF3nN-8LIX1u6I3_N239sctp0w",
+    authDomain: "dmmpr1d.firebaseapp.com",
+    projectId: "dmmpr1d",
+    storageBucket: "dmmpr1d.appspot.com",
+    messagingSenderId: "61747192919",
+    appId: "1:61747192919:web:c11c531638d58ae8afaf12"
+  });
